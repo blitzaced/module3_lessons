2717c9f7f366347d72ae90da64a84fa71cec1f02
Will Green <will.green.mha.pmp@gmail.com>
2024-12-11 11:57:10 -0600

    save

---
+++ module3_lesson3.py
@@ -0,0 +1,33 @@
+# Intro to ORM's
+
+"""
+    An Object-Relational Mapper (ORM) is a tool that allows developers to interact with database using object-oriented programming (OOP)
+    concepts. It abstracts SQL commands, letting you work with database entities as if they were regular objects in your programming
+    language. This simplifies database interaction by aloowing developers to work with data in a way that's familiar to them in a
+    language such as Python, without writing raw SQL querires.
+    
+    Why Use an ORM?
+        1. Easf of Use: Instead of writing SQL queries, you manipulate Python objects.
+        2. Portability: ORMs allow you to write database-agnostic code, making it easier to switch between database systems
+        (MySWL, PostgreSQL, SQLite).
+        3. Security: ORM libraries usually come with built-in protections against SQL injection attacks.
+        4. Productivity: You spend less time writing boilerplate code for database interaction.
+        
+    Basic Concepts of ORMs
+        - Model: Each model in an ORM corresponds to a table in your database. Attributes of the model correspond to columns.
+        - Instance: An instance of a model represents a row/record in the table.
+        - Query: ORM libraries provide query builders that allow you to retrieve, filter, and manipulate data.
+"""        
+
+# SQLAlchemy 2.0: Python's Most Popular ORM
+
+"""
+    SQLAlchemy is a powerful and flexible ORM that allows you to perform database operations in Python. Version 2.0 brings
+    several improvements, including more modern and expressive syntax, and improved type hinting and async support.
+    
+    
+    Virtual Environments - a virtual environment is an isolated workspace for Python projects that allows you to manage tools
+    (packages) for a specific project.
+
+
+"""
